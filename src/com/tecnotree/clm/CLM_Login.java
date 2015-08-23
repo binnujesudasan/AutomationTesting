5c54d4440aac91ec18bca27cad93533eec51419a
jesudbi <binnu.jesudasan@tecnotree.com>
2015-08-23 23:05:07 +0530

    Third Commit

--- src/com/tecnotree/clm/CLM_Login.java
+++ src/com/tecnotree/clm/CLM_Login.java
@@ -1,5 +1,21 @@
 package com.tecnotree.clm;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class CLM_Login {
-	
+	@Test
+	public void testLaunch(){
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://tecnotree.com");
+		driver.findElement(By.id("Search_header")).clear();
+		driver.findElement(By.id("Search_header")).sendKeys("CLM");
+		driver.findElement(By.name("SearchButton")).click();
+	}
 }
