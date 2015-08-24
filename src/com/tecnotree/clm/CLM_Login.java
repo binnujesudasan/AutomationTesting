5c3f67677bedc59008f458913b317ac51205b7e9
jesudbi <binnu.jesudasan@tecnotree.com>
2015-08-24 12:42:54 +0530

    Fourth Commit

--- src/com/tecnotree/clm/CLM_Login.java
+++ src/com/tecnotree/clm/CLM_Login.java
@@ -2,14 +2,17 @@ package com.tecnotree.clm;
 
 import java.util.concurrent.TimeUnit;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CLM_Login {
+	@SuppressWarnings("deprecation")
 	@Test
-	public void testLaunch(){
+	public void testLaunch() throws InterruptedException{
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -17,5 +20,7 @@ public class CLM_Login {
 		driver.findElement(By.id("Search_header")).clear();
 		driver.findElement(By.id("Search_header")).sendKeys("CLM");
 		driver.findElement(By.name("SearchButton")).click();
+		Thread.sleep(3000);
+		Assert.assertTrue(driver.getPageSource().contains("Search for \"CLM\" returned 5 matches"));
 	}
 }
