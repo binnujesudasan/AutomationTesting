7d0292746c4b4265796405adb9ad5c9a12d77b27
jesudbi <binnu.jesudasan@tecnotree.com>
2015-08-27 15:24:45 +0530

    converteed to single method

--- src/com/tecnotree/clm/CLM_Login.java
+++ src/com/tecnotree/clm/CLM_Login.java
@@ -22,22 +22,19 @@ public class CLM_Login {
 		driver.get("http://tecnotree.com");
 	}
 	@Test
-	public void testLaunch() throws InterruptedException{
+	public void test1() throws InterruptedException{
 		driver.findElement(By.id("Search_header")).clear();
 		driver.findElement(By.id("Search_header")).sendKeys("CLM");
 		driver.findElement(By.name("SearchButton")).click();
 		Thread.sleep(3000);
 		Assert.assertTrue(driver.getPageSource().contains("Search for \"CLM\" returned 5 matches"));
 		Thread.sleep(3000);
-	}
-	@Test
-	public void clickResultRandomly(){
 		List<WebElement> links = driver.findElements(By.xpath("//p[@class='url']/a"));
 		int randomNumber = 0;
 		if(links.size() > 0){
 			randomNumber = (int) (Math.random() * links.size());
 		}
-		links.get(randomNumber).click();
+		links.get(randomNumber+1).click();
 		System.out.println(links.get(randomNumber));
 	}
 }
